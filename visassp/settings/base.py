86b498a929b38a2edbae18d24a504b20c85f1573
ushergui <guilherme-02-08@hotmail.com>
2021-08-01 16:19:37 -0300

    4

--- visassp/settings/base.py
+++ visassp/settings/base.py
@@ -76,22 +76,13 @@ WSGI_APPLICATION = 'visassp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', 'visa'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASS', 'admin'),
-        'HOST': 'localhost',
-        #'PORT': '5432',
-    }
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
 
 
 # Password validation
