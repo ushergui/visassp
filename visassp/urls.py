feffc5156a01ff6d5fcb5dedda9d0c816556f340
ushergui <guilherme-02-08@hotmail.com>
2021-08-10 02:47:42 -0300

    sda

--- visassp/urls.py
+++ visassp/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from django.conf.urls.static import static
 
-import visassp.visassp.settings.base
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
